55667306b36907e0f66607e6d0775f33926dd4f0
TobyComon <toby.comon@gmail.com>
2022-05-02 12:03:44 -0600

    fasdfasdf

--- Planit.client/src/env.js
+++ Planit.client/src/env.js
@@ -1,6 +1,7 @@
+
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
+export const domain = 'dev-kpqs6nam.us.auth0.com'
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const audience = 'https://TobyDev.com'
+export const clientId = 'Ets60VsVsIMAIrrwUachpNXC3rBdIpFX'
